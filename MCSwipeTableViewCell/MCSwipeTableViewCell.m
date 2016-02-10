23d80453fb39f5718619afcee297b705f85cab4a
Attila Bardos <attila@hyper.no>
2016-02-10 14:18:02 +0100

    Fix compiler warnings

--- MCSwipeTableViewCell/MCSwipeTableViewCell.m
+++ MCSwipeTableViewCell/MCSwipeTableViewCell.m
@@ -337,7 +337,7 @@ typedef NS_ENUM(NSUInteger, MCSwipeTableViewCellDirection) {
         UIPanGestureRecognizer *g = (UIPanGestureRecognizer *)gestureRecognizer;
         CGPoint point = [g velocityInView:self];
         
-        if (fabsf(point.x) > fabsf(point.y) ) {
+        if (fabs(point.x) > fabs(point.y) ) {
             if (point.x < 0 && !_modeForState3 && !_modeForState4) {
                 return NO;
             }
@@ -434,7 +434,7 @@ typedef NS_ENUM(NSUInteger, MCSwipeTableViewCellDirection) {
     }
     
     else if (percentage < 0 && percentage > -_firstTrigger) {
-        alpha = fabsf(percentage / _firstTrigger);
+        alpha = fabs(percentage / _firstTrigger);
     }
     
     else {
